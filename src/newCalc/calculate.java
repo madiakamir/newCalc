3757f94e759cd9de001be7db6f2dcc7b65fb2ba2
madiakamir <madiakamir@gmail.com>
2017-06-25 18:31:11 +0300

    firstCommit

---
+++ src/newCalc/calculate.java
@@ -0,0 +1,13 @@
+package newCalc;
+import java.util.Scanner;
+
+
+/**
+ * Created by Tom on 25.06.2017.
+ */
+public class calculate {
+    Scanner reader = new Scanner(System.in);
+    int number1 = reader.nextInt();
+    int number2 =  reader.nextInt();
+
+}
